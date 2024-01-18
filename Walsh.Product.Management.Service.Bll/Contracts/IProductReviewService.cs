f90fdec4abc6a777175aec45f13bd18be590ec4c
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 00:36:11 +0200

    feat: add methods for product review on bll service

---
+++ Walsh.Product.Management.Service.Bll/Contracts/IProductReviewService.cs
@@ -0,0 +1,11 @@
+﻿using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Contracts
+{
+    public interface IProductReviewService
+    {
+        Task<ProductReviewModel> GetProductReviewAsync(int productId);
+        Task<ProductReviewModel> CreateProductReviewAsync(ProductReviewModel model);
+        Task<ProductReviewModel> UpdateProductReviewAsync(ProductReviewModel model);
+    }
+}
---
+++ Walsh.Product.Management.Service.Bll/Implimentations/ProductReviewService.cs
@@ -0,0 +1,21 @@
+﻿using Walsh.Product.Management.Service.Bll.Contracts;
+using Walsh.Product.Management.Service.Dal.Contracts;
+using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Implimentations
+{
+    public class ProductReviewService: IProductReviewService
+    {
+        private readonly IProductReviewDataAccess _productReviewDataAccess;
+        public ProductReviewService(IProductReviewDataAccess productReviewDataAccess)
+        {
+            _productReviewDataAccess = productReviewDataAccess;
+        }
+
+        public Task<ProductReviewModel> CreateProductReviewAsync(ProductReviewModel model) => _productReviewDataAccess.CreateProductReviewAsync(model); 
+
+        public Task<ProductReviewModel> GetProductReviewAsync(int productId) => _productReviewDataAccess.GetProductReviewAsync(productId);
+
+        public Task<ProductReviewModel> UpdateProductReviewAsync(ProductReviewModel model) => _productReviewDataAccess.UpdateProductReviewAsync(model);
+    }
+}
