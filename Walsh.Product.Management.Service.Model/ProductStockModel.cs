fd159d8f7430107a719600a9b1256ea0c8cb421e
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 20:41:51 +0200

    feat: Add product in stock model

---
+++ Walsh.Product.Management.Service.Model/ProductStockModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Walsh.Product.Management.Service.Model
+{
+    public class ProductStockModel
+    {
+        public int StockId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductInStock { get; set; }
+    }
+}
