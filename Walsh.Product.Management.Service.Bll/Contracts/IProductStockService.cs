6781ac53593010bf0d6a86fa1f27c0a924422d83
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 00:40:39 +0200

    feat: add methods for product in stock under bll service

---
+++ Walsh.Product.Management.Service.Bll/Contracts/IProductStockService.cs
@@ -0,0 +1,11 @@
+﻿using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Contracts
+{
+    public interface IProductStockService
+    {
+        Task<ProductStockModel> GetProductInStockAsync(int productId);
+        Task<ProductStockModel> CreateProductInStockAsync(ProductStockModel model);
+        Task<ProductStockModel> UpdateProductInStockAsync(ProductStockModel model);
+    }
+}
---
+++ Walsh.Product.Management.Service.Bll/Implimentations/ProductStockService.cs
@@ -0,0 +1,21 @@
+﻿using Walsh.Product.Management.Service.Bll.Contracts;
+using Walsh.Product.Management.Service.Dal.Contracts;
+using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Implimentations
+{
+    public class ProductStockService : IProductStockService
+    {
+        private readonly IProductStockDataAccess _productStockDataAccess;
+        public ProductStockService(IProductStockDataAccess productStockDataAccess)
+        {
+            _productStockDataAccess = productStockDataAccess;
+        }
+
+        public Task<ProductStockModel> CreateProductInStockAsync(ProductStockModel model) => _productStockDataAccess.CreateProductInStockAsync(model);
+
+        public Task<ProductStockModel> GetProductInStockAsync(int productId) => _productStockDataAccess.GetProductInStockAsync(productId);
+
+        public Task<ProductStockModel> UpdateProductInStockAsync(ProductStockModel model) => _productStockDataAccess.UpdateProductInStockAsync(model);
+    }
+}
