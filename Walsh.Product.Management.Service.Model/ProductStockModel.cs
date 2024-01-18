13d029b30dc877e3bc7733b389668abea65462ba
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 23:41:20 +0200

    feat: add isDelete, createdAt and updatedAt from at product instock model

--- Walsh.Product.Management.Service.Model/ProductStockModel.cs
+++ Walsh.Product.Management.Service.Model/ProductStockModel.cs
@@ -5,5 +5,8 @@
         public int StockId { get; set; }
         public int ProductId { get; set; }
         public string ProductInStock { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
     }
 }
