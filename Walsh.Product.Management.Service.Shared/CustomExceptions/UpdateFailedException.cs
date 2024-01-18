e6060f1d9322dc11025b3044b8bf7736d846a4dd
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 22:50:09 +0200

    feat: add custom exception for update

---
+++ Walsh.Product.Management.Service.Shared/CustomExceptions/UpdateFailedException.cs
@@ -0,0 +1,17 @@
+﻿namespace Walsh.Product.Management.Service.Shared.CustomExceptions
+{
+    public class UpdateFailedException : Exception
+    {
+        public UpdateFailedException() : base("Failed to update the entity.")
+        {
+        }
+
+        public UpdateFailedException(string message) : base(message)
+        {
+        }
+
+        public UpdateFailedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
