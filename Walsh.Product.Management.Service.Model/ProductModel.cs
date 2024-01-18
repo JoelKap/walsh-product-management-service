cc359204b24cdefd13326c179351b03f49c0b6e7
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 01:36:27 +0200

    feat: add methods for product in product controller, in this commit also included validations

--- Walsh.Product.Management.Service.Model/ProductModel.cs
+++ Walsh.Product.Management.Service.Model/ProductModel.cs
@@ -15,5 +15,42 @@
         public DateTime UpdateAt { get; set; }
         public ICollection<ProductReviewModel> Reviews { get; set; }
         public ProductStockModel Stock { get; set; }
+
+        public bool Validate(out List<string> messages)
+        {
+            messages = new List<string>();
+
+            if (LocationId is 0)
+            {
+                messages.Add("Location is missing");
+            }
+
+            if (CategoryId is 0)
+            {
+                messages.Add("Category is missing");
+            }
+
+            if (CategoryId is 0)
+            {
+                messages.Add("Category is missing");
+            }
+
+            if (string.IsNullOrWhiteSpace(ProductTitle))
+            {
+                messages.Add("Product title is empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(ProductDescription))
+            {
+                messages.Add("Product description is empty");
+            }
+
+            if (string.IsNullOrWhiteSpace(ProductImageUrl))
+            {
+                messages.Add("Image Url is empty");
+            }
+
+            return (messages.Any() == false);
+        }
     }
 }
--- Walsh.Product.Management.Service/Controllers/ProductController.cs
+++ Walsh.Product.Management.Service/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Walsh.Product.Management.Service.Bll.Contracts;
 using Walsh.Product.Management.Service.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,52 +10,75 @@ namespace Walsh.Product.Management.Service.Api.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
         // GET: api/<ProductController>
         [HttpGet]
-        public List<ProductModel> Get()
+        public IActionResult Get()
         {
-           return new List<ProductModel>();
+           return Ok(_productService.GetProducts());
         }
 
         // GET api/<ProductController>/5
         [HttpGet("{productId}")]
         public IActionResult GetProduct(int productId)
         {
-            return Ok(new { productId });
+            return Ok(_productService.GetProductAsync(productId));
         }
 
         // GET api/<ProductController>/str
         [HttpGet("searchProducts")]
         public IActionResult SearchProduct(string searchStr)
         {
-            return null;
+            return Ok(_productService.SearchProducts(searchStr));
         }
 
         // POST api/<ProductController>
         [HttpPost()]
         public IActionResult Post(ProductModel model)
         {
-            return Ok(true);
+            if (model.Validate(out List<string> messages) == false)
+            {
+                return BadRequest(string.Join(Environment.NewLine, messages));
+            }
+
+            return Ok(_productService.CreateProductAsync(model));
         }
 
         // PUT api/<ProductController>
         [HttpPut()]
         public IActionResult Put([FromBody] ProductModel model)
         {
-            return Ok(true);
+            if (model.Validate(out List<string> messages) == false)
+            {
+                return BadRequest(string.Join(Environment.NewLine, messages));
+            }
+
+            return Ok(_productService.UpdateProductAsync(model));
         }
 
         // PUT api/<ProductController>
         [HttpPut("LikeOrUnlikeProduct")]
-        public IActionResult LikeorUnlikeProduct([FromBody] ProductModel model)
-        {  
-            return Ok(model);
+        public IActionResult LikeOrUnlikeProduct([FromBody] ProductModel model)
+        {
+            if (model.Validate(out List<string> messages) == false)
+            {
+                return BadRequest(string.Join(Environment.NewLine, messages));
+            }
+
+            return Ok(_productService.LikeOrUnlikeProductAsync(model));
         }
 
         // DELETE api/<ProductController>/5
         [HttpDelete("{productId}")]
         public IActionResult Delete(int productId)
         {
+            _productService.DeleteProductAsync(productId);
             return NoContent();
         }
     }
