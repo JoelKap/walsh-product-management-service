23c51971fb5839eae304fda1332dbfa4180a5d5e
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 21:30:07 +0200

    feat: Add mappings between data transfer objects to data model and vice versa

---
+++ Walsh.Product.Management.Service.Dal/Mappings/MappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Walsh.Product.Management.Service.Dal.DTO;
+using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Dal.Mappings
+{
+    public class MappingProfile
+    {
+            public static IMapper MapperConfiguration()
+            {
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<List<Walsh.Product.Management.Service.Dal.DTO.Product>, List<ProductModel>>().ReverseMap();
+                    cfg.CreateMap<Walsh.Product.Management.Service.Dal.DTO.Product, ProductModel>().ReverseMap();
+                    cfg.CreateMap<ProductCategory, ProductCategoryModel>().ReverseMap();
+                    cfg.CreateMap<ProductLocation, ProductLocationModel>().ReverseMap();
+                    cfg.CreateMap<ProductReview, ProductReviewModel>().ReverseMap();
+                    cfg.CreateMap<ProductStock, ProductStockModel>().ReverseMap();
+                });
+
+                IMapper mapper = config.CreateMapper();
+                return mapper;
+            }
+    }
+}
