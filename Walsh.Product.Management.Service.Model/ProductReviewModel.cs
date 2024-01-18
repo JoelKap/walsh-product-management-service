d9aed9ebe3b2d51afb113b2a646568b10ef9d8e7
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 20:37:58 +0200

    feat: Add product review model

---
+++ Walsh.Product.Management.Service.Model/ProductReviewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Walsh.Product.Management.Service.Model
+{
+    public class ProductReviewModel
+    {
+        public int ReviewId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductRating { get; set; } 
+        public string ProductReview { get; set; }
+    }
+}
