8ae378a344d1a3eb72d1488e0c847966c35dc475
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 10:52:43 +0200

    feat: Add interface methods for Product

---
+++ Walsh.Product.Management.Service.Dal/Contracts/IProductDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Dal.Contracts
+{
+    public interface IProductDataAccess
+    {
+        IEnumerable<ProductModel> GetProducts(); 
+        Task<ProductModel> GetProduct(int productId);
+        IEnumerable<ProductModel> SearchProducts(string searchStr);
+        Task CreateProductAsync(ProductModel model); 
+        Task UpdateProductAsync(ProductModel model); 
+        Task LikeOrUnlikeProductAsync(ProductModel model);
+        Task DeleteProductAsync(int productId);
+    } 
+}
