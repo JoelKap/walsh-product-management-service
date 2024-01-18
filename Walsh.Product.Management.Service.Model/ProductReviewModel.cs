4c183f7d08bc7fd5be96dc1d807a90fb49b1dcd9
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 23:40:49 +0200

    feat: add isDelete, createdAt and updatedAt from at product review model

--- Walsh.Product.Management.Service.Model/ProductReviewModel.cs
+++ Walsh.Product.Management.Service.Model/ProductReviewModel.cs
@@ -6,5 +6,8 @@
         public int ProductId { get; set; }
         public int ProductRating { get; set; } 
         public string ProductReview { get; set; }
+        public bool IsDeleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
     }
 }
