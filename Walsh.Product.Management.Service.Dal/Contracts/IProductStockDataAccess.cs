346e1123d28c728f24b4c9ec43d23d61c42d6a40
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-18 23:39:27 +0200

    feat: [wip] Implimenting dal services for product in stock class

---
+++ Walsh.Product.Management.Service.Dal/Contracts/IProductStockDataAccess.cs
@@ -0,0 +1,11 @@
+﻿using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Dal.Contracts
+{
+    public interface IProductStockDataAccess
+    { 
+        Task<ProductStockModel> GetProductInStock(int productId);
+        Task<ProductStockModel> CreateProductInStockAsync(ProductModel model);
+        Task<ProductStockModel> UpdateProductInStockAsync(ProductModel model);
+    }
+}
---
+++ Walsh.Product.Management.Service.Dal/Implimentations/ProductStockDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Walsh.Product.Management.Service.Dal.Contracts;
+using Walsh.Product.Management.Service.Dal.DTO;
+using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Dal.Implimentations
+{
+    public class ProductStockDataAccess: IProductStockDataAccess
+    {
+        private readonly WalshDbContext _walshContext;
+        private readonly IMapper _mapper;
+        public ProductStockDataAccess(WalshDbContext walshContext)
+        {
+            _walshContext = walshContext;
+            _mapper = Mappings.MappingProfile.MapperConfiguration();
+        }
+
+        public Task<ProductStockModel> CreateProductInStockAsync(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductStockModel> GetProductInStock(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductStockModel> UpdateProductInStockAsync(ProductModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
