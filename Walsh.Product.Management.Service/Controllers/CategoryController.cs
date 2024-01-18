ba8618abe6ecca3a333cc18a8d6baf70a54e38b1
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 00:53:08 +0200

    feat: implemented get categories from controller

--- Walsh.Product.Management.Service/Controllers/CategoryController.cs
+++ Walsh.Product.Management.Service/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Walsh.Product.Management.Service.Bll.Contracts;
 using Walsh.Product.Management.Service.Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -9,25 +10,17 @@ namespace Walsh.Product.Management.Service.Api.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly IProductCategoryService _productCategoryService;
+
+        public CategoryController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
         // GET: api/<CategoryController>
         [HttpGet]
-        public List<ProductCategoryModel> Get()
+        public IActionResult Get()
         {
-            var dummyCategories = new List<ProductCategoryModel>() {
-                new ProductCategoryModel(){
-                        CategoryId = 1,
-                        CategoryName = "Books"
-                        },
-                        new ProductCategoryModel(){
-                        CategoryId = 2,
-                        CategoryName = "Movies"
-                        },
-                        new ProductCategoryModel(){
-                        CategoryId = 3,
-                        CategoryName = "Games"
-                   }
-            };
-            return dummyCategories;
+            return Ok(_productCategoryService.GetCategories());
         }
 
     }
