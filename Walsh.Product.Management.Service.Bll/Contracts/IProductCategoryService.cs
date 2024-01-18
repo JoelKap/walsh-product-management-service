5d5b1b58440ef9d7f8fed191bcbe0160f80129fc
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 00:44:24 +0200

    feat: add get categories method from bll service

---
+++ Walsh.Product.Management.Service.Bll/Contracts/IProductCategoryService.cs
@@ -0,0 +1,9 @@
+﻿using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Contracts
+{
+    public interface IProductCategoryService
+    {
+        IEnumerable<ProductCategoryModel> GetCategories();
+    }
+}
---
+++ Walsh.Product.Management.Service.Bll/Implimentations/ProductCategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Walsh.Product.Management.Service.Bll.Contracts;
+using Walsh.Product.Management.Service.Dal.Contracts;
+using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Implimentations
+{
+    public class ProductCategoryService: IProductCategoryService
+    {
+        private readonly IProductCategoryDataAccess _productCategoryDataAccess;
+
+        public ProductCategoryService(IProductCategoryDataAccess productCategoryDataAccess)
+        {
+             _productCategoryDataAccess = productCategoryDataAccess;
+        }
+
+        public IEnumerable<ProductCategoryModel> GetCategories() => _productCategoryDataAccess.GetCategories();
+    }
+}
