3adab52828d9abd9bc5f8c8bfafdb753aa594e94
Joel Kapuku <joel.kapuku@standardbank.co.za>
2024-01-19 00:49:01 +0200

    feat: add method for get locations in bll

---
+++ Walsh.Product.Management.Service.Bll/Contracts/IProductLocationService.cs
@@ -0,0 +1,9 @@
+﻿using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Contracts
+{
+    public interface IProductLocationService
+    {
+        IEnumerable<ProductLocationModel> GetLocations();
+    }
+}
---
+++ Walsh.Product.Management.Service.Bll/Implimentations/ProductLocationService.cs
@@ -0,0 +1,17 @@
+﻿using Walsh.Product.Management.Service.Bll.Contracts;
+using Walsh.Product.Management.Service.Dal.Contracts;
+using Walsh.Product.Management.Service.Model;
+
+namespace Walsh.Product.Management.Service.Bll.Implimentations
+{
+    public class ProductLocationService: IProductLocationService
+    {
+        private readonly IProductLocationDataAccess _productLocationDataAccess;
+        public ProductLocationService(IProductLocationDataAccess productLocationDataAccess)
+        {
+            _productLocationDataAccess = productLocationDataAccess;
+        }
+
+        public IEnumerable<ProductLocationModel> GetLocations() => _productLocationDataAccess.GetLocations();
+    }
+}
